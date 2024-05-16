e7831ca7495596b63c30565a020056f26914b29a
Pryme523 <boakyeemmanuel523@gmail.com>
2024-05-16 08:45:18 -0700

    Add more variations of arithmetric operations

---
+++ C-programming/arithmetric_char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - performs basic arithmetric operations 
+ * 		on given numbers
+ *
+ * Return 0 Always
+ */
+
+int main (void) 
+
+{
+	/** Variable declaration and initialization*/
+	char a = 'e';
+	int b = 48;
+
+	printf("Operand 1: %d, Operand 2: %d\n", a, b);
+	printf("Addition of Operand 1 and Operand 2: %d\n", a + b);
+	printf("Multiplication of Operand 1 and Operand 2: %d\n", a * b);
+	printf("Subtraction of Operand 1 and Operand 2: %d\n", a - b);
+	printf("Division of Operand 1 and Operand 2: %d\n", a / b);
+
+
+	return (0);
+}
---
+++ C-programming/char_values.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - gives decimal value attached to char
+ * 		char variables
+ * 		
+ *
+ * Return 0 Always
+ */
+
+int main (void) 
+
+{
+	/** Variable declaration and initialization*/
+	char a = 'a';
+	char b = 'b';
+	char c = 'c';
+	char d = 'd';
+	char e = 'e';
+
+	printf("Value of char a:%d\n", a);
+	printf("Value of char b:%d\n", b);
+	printf("Value of char c:%d\n", c);
+	printf("Value of char d:%d\n", d);
+	printf("Value of char e:%d\n", e);
+
+	return (0);
+}

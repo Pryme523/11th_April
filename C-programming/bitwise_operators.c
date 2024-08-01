20ae0c4a1201bdfdf8d94bf7a708d1d8ef97f582
Pryme523 <boakyeemmanuel523@gmail.com>
2024-08-01 06:16:11 -0700

    Update bitwise operators

---
+++ C-programming/bitwise_operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int a = 34, c;
+
+	c = a << 4;
+	printf("%d", c);
+	return 0;
+}
---
+++ C-programming/while_print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints positve numbers
+ * from 0 - 9
+ *
+ * Return 0 Always
+ */
+
+int main (void)
+{
+	int a = 0;
+
+	while(a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+}

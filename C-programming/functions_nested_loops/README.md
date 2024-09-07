80c1b1cb3e46ddb67608cf66bff53873d0e5f523
Pryme523 <boakyeemmanuel523@gmail.com>
2024-09-07 16:49:36 -0700

    Track all files

---
+++ C-programming/functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory that is meant to house all of my alx practice on functions and nested loops
---
+++ C-programming/functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _isalpha(int c);
+int _putchar(char c);
+
+#endif

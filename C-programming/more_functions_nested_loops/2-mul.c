b3a40e2b1b1440e8c100561fa79c06bed17196e7
Pryme523 <boakyeemmanuel523@gmail.com>
2024-09-10 07:49:51 -0700

    Update 2-mul.c

---
+++ C-programming/more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - multiplies two integers
+ *
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	_putchar(product + '0');
+	
+		return (0);
+}
---
+++ C-programming/more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory containing all of the alx tasks solved on more functions and nested loops starting from task 2

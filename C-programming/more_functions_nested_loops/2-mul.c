7f9d34d508cbaff6532ea262237604dede243ca3
Pryme523 <boakyeemmanuel523@gmail.com>
2024-09-10 14:36:37 -0700

    Update 2-mul.c

--- C-programming/more_functions_nested_loops/2-mul.c
+++ C-programming/more_functions_nested_loops/2-mul.c
@@ -9,7 +9,5 @@ int mul(int a, int b)
 {
 	int product = a * b;
 
-	_putchar(product + '0');
-	
-		return (product);
+	return (product);
 }
--- C-programming/more_functions_nested_loops/main.h
+++ C-programming/more_functions_nested_loops/main.h
@@ -1,2 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 int _putchar(char c);
 int mul(int a, int b);
+
+#endif
